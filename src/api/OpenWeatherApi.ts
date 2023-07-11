867ea98ad68f6170fb6d915bf27d6e8d8dedc191
david0411 <tlyeyd@gmail.com>
2023-07-11 23:19:18 +0800

    done

--- src/api/OpenWeatherApi.ts
+++ src/api/OpenWeatherApi.ts
@@ -6,9 +6,7 @@ class OpenWeatherApi {
     static cityCode = "1819730"
     static appid = "336226ba578c3b386bead938bce9b09d"
     static apiUrl = `https://api.openweathermap.org/data/2.5/forecast?id=${OpenWeatherApi.cityCode}&units=metric&appid=${OpenWeatherApi.appid}`
-    static fetchForecastWeatherData = (
-        (setApiResult2ForecastData:
-             (forecastResultData: ForecastData) => void) => {
+    static fetchForecastWeatherData = ((setApiResult2ForecastData: (forecastResultData: ForecastData) => void) => {
             axios.get(OpenWeatherApi.apiUrl)
                 .then(
                     (response: AxiosResponse<ForecastData>) => setApiResult2ForecastData(response.data)
--- src/ui/page/forecastPage/component/ForecastPageHeader.tsx
+++ src/ui/page/forecastPage/component/ForecastPageHeader.tsx
@@ -1,32 +1,34 @@
 import "../css/ForecastPageHeaderStyle.css"
 import React from "react";
 
-type Props =    {
-    updateTime: string |undefined
+type Props = {
+    updateTime: string | undefined
     fetchForecastWeatherData: () => void
 }
 
 class ForecastPageHeader extends React.Component<Props> {
     render() {
-        return (
-            <>
+        return <>
                 <div className="d-flex justify-content-between">
                     <h1 className="headerStyle1">
                         5 Day / 3 Hour Forecast
                     </h1>
                     <div className="d-flex">
                         <div className="headerStyle2">Last Update Time: <br/>
-                            11/7/2023 11:21:00
+                            {
+                                this.props.updateTime
+                                ? this.props.updateTime
+                                    : "loading"
+                            }
                         </div>
                         <div className="emptySpace"></div>
-                        <div id="reloadButton"></div>
+                        <div id="reloadButton" onClick={this.props.fetchForecastWeatherData}></div>
                     </div>
                 </div>
                 <div className="headerStyle3">
                     Hong Kong
                 </div>
             </>
-        )
     }
 
 
